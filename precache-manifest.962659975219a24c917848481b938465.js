098906ce30d4f79aac3d4204b6b253ca9f037430
Krzysztof Kornakiewicz <krzysztof.kornakiewicz@travelperk.com>
2020-07-02 14:44:15 +0200

    Updates

---
+++ precache-manifest.962659975219a24c917848481b938465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f7c45f9a0be6cb166ef9ee03d398da",
+    "url": "/fe-exercise/index.html"
+  },
+  {
+    "revision": "94b062a62c3a06f7468e",
+    "url": "/fe-exercise/static/css/2.6a7ab022.chunk.css"
+  },
+  {
+    "revision": "6d56ccacb14fcc7e90b4",
+    "url": "/fe-exercise/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "94b062a62c3a06f7468e",
+    "url": "/fe-exercise/static/js/2.26a6457e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fe-exercise/static/js/2.26a6457e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d56ccacb14fcc7e90b4",
+    "url": "/fe-exercise/static/js/main.cfded2e3.chunk.js"
+  },
+  {
+    "revision": "24ec400b11405a98e8eb",
+    "url": "/fe-exercise/static/js/runtime-main.4d1fd165.js"
+  }
+]);
